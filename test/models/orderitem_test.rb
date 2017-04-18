797eb293ae82ab6e6a04abaeacfbda113f2b3952
sai samant <saisamant@gmail.com>
2017-04-18 16:50:34 -0700

    order item model and test outline

---
+++ test/models/orderitem_test.rb
@@ -0,0 +1,26 @@
+require "test_helper"
+
+describe Orderitem do
+
+
+  it "quantity must be greater than zero" do
+    
+  end
+
+  it 'quantity must be an integer' do
+
+  end
+
+  it 'must be present' do
+
+  end
+
+  it 'must belong to an order' do
+    skip
+  end
+
+  it 'must belong to a Product' do
+    skip
+  end
+
+end
