ab2d6900cd13fabbf1607749b6f4fb7c39582788
Anacelia Gillette <acgillette426@gmail.com>
2017-04-18 16:09:09 -0700

    Create product test green

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,7 +3,11 @@ require "test_helper"
 describe Product do
   let(:product) { Product.new }
 
-  it "must be valid" do
-    value(product).must_be :valid?
+  it "you can create a new Product" do
+    product.name = "TestName"
+    product.price = 5.00
+    product.quantity = 4
+    product.valid?.must_equal true
+
   end
 end
