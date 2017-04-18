c2b2939247b0f7051be20dc277514e4fb03e890c
jjousun <jjousun@gmail.com>
2017-04-18 16:08:41 -0700

    Created merchant model, added columns to merchants table, started on tests for model

---
+++ test/models/merchant_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+describe Merchant do
+  let(:merchant) { Merchant.new }
+
+  #Username must be present
+  #Username must be unique
+  #Email Address must be present
+  #Email Address must be unique
+end
