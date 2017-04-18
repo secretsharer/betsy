12a246a02bb7477a59d820cf62fb6876a9475e50
jjousun <jjousun@gmail.com>
2017-04-18 16:29:20 -0700

    Wrote validation tests for Merchant model

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -3,8 +3,44 @@ require "test_helper"
 describe Merchant do
   let(:merchant) { Merchant.new }
 
-  #Username must be present
-  #Username must be unique
-  #Email Address must be present
-  #Email Address must be unique
+  describe 'validations' do
+    it 'requires a username to be valid' do
+      #merchant with no username is bad
+      merchant = Merchant.new(email: 'hey@ada.org')
+      merchant.valid?.must_equal false
+      merchant.errors.messages.must_include :username
+      #merchant with username is good
+      merchant = merchants(:dan)
+      merchant.valid?.must_equal true
+      merchant.errors.message.wont_include :username
+    end
+
+    it 'requires a unique username to be valid' do
+      dan = merchants(:dan)
+
+      user = Merchant.new(username: 'dan', email: 'ho@ada.org')
+      result = user.save
+      result.must_equal false
+      user.errors.messages.must_include :username
+    end
+
+    it 'requires an email address to be valid' do
+      merchant = Merchant.new(username: 'sai')
+      merchant.valid?.must_equal false
+      merchant.errors.messages.must_include :email
+
+      merchant = merchants(:dan)
+      merchant.valid?.must_equal true
+      merchant.errors.message.wont_include :email
+    end
+
+    it 'requires a unique email address to be valid' do
+      dan = merchants(:dan)
+
+      user = Merchant.new(username: 'sai', email: 'dan@ada.org')
+      result = user.save
+      result.must_equal false
+      user.errors.messages.must_include :email
+    end
+  end
 end
