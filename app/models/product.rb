d4e49d74936f98088e87059bd21232f5e08611d8
Anacelia Gillette <acgillette426@gmail.com>
2017-04-18 16:07:15 -0700

    Made yml files for fixtures

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
-  
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true
+  validates_numericality_of :price, greater_than: 0
 end
