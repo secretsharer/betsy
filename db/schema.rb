95db2c5782743b4b0431c096918a0345a18c6e44
Anacelia Gillette <acgillette426@gmail.com>
2017-04-19 15:39:12 -0700

    Added validayiond for product/order and review models

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 20170418234636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "orderitems", force: :cascade do |t|
-    t.integer  "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "merchants", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -28,15 +22,21 @@ ActiveRecord::Schema.define(version: 20170418234636) do
     t.string   "username"
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.float    "price"
+  create_table "orderitems", force: :cascade do |t|
     t.integer  "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string  "name"
+    t.float   "price"
+    t.integer "quantity"
   end
 
   create_table "reviews", force: :cascade do |t|
