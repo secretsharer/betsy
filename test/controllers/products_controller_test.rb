18bc43b4ef274ede285628e68a1f374ce73f4eac
Anacelia Gillette <acgillette426@gmail.com>
2017-04-21 13:52:52 -0700

    Got nested routes products controller green

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -7,7 +7,7 @@ describe ProductsController do
   end
 
   it "should get the index if merchant_id is passed as params" do
-    get merchant_products_path
+    get merchant_products_path(merchants(:dan).id)
     must_respond_with :success
   end
 end
