e5b4e33151c9a13801a48c771b9a141b97841142
jjousun <jjousun@gmail.com>
2017-04-21 08:53:43 -0700

    Fixed merge conflict in Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,8 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates_numericality_of :price, greater_than: 0
 
-<<<<<<< HEAD
   belongs_to :merchant, :foreign_key => 'merchant_id'
-=======
->>>>>>> 2d3ca3d8a76911fb2c852f713c354f147e42cf7d
+
   has_many :reviews
   has_many :orderitems
   has_and_belongs_to_many :categories
