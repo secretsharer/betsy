83fb77de74b50b6be0cae2c16b86df6c38ca6c44
sai samant <saisamant@gmail.com>
2017-04-21 12:44:45 -0700

    nested route for merchant's products; updated index view to list merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :merchants
+  resources :merchants do
+    resources :products, only: [:index]
+  end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
