6815aea7c48ef2a3281f66d6eabb17e845516664
Anacelia Gillette <acgillette426@gmail.com>
2017-04-21 13:41:41 -0700

    Made product routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :products, only: [:index]
   end
 
+  resources :products
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
