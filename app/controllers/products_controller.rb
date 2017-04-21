39265e0c3e43b0d7570393a463459c854492ce82
Anacelia Gillette <acgillette426@gmail.com>
2017-04-21 15:30:21 -0700

    Added quantity validation and tests to product model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,4 +16,12 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def update
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :quantity)
+  end
 end
