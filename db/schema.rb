b19e97a2503206c2c6ea5fcd21a42cda00ecd77e
Olivia <ssecretsharer@gmail.com>
2017-04-23 15:35:31 -0700

    Fixed the multiple names, nameError problem for Cart vs Order. Also made new OrdersController

--- db/schema.rb
+++ db/schema.rb
@@ -54,10 +54,12 @@ ActiveRecord::Schema.define(version: 20170422143427) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string  "name"
-    t.float   "price"
-    t.integer "quantity"
-    t.integer "merchant_id"
+    t.string   "name"
+    t.float    "price"
+    t.integer  "quantity"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "merchant_id"
   end
 
   create_table "reviews", force: :cascade do |t|
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,27 +1,27 @@
 require "test_helper"
 
-describe CartsController do
+describe OrdersController do
 
   it "should get cart 'index'" do
     #empty cart page presumably showing one empty cart
-    get cart_path
+    get orders_path
     must_respond_with :success
   end
 
   it "should show orderitems if merchant (session?) has orderitems" do
     #how should this path look? products per merchant, per session?
-    get cart_path(merchants(:dan).id(orderitems))
+    get order_path(merchants(:dan).id(orderitems))
     must_respond_with :success
   end
 
-  it "adding items should update the cart orderitem count" do
+  it "adding items should update the order orderitem count" do
     #I'm guessing I don't need one of these, but here they both are.
     #patch cart_path(orderitems(:banana).id), params: {orderitem: { quantity: 3 } }
     proc {
     patch orderitem_path(products(:banana).id)
   }.must_change 'Orderitem.count', quantity: 3
   must_respond_with :redirect
-  must_redirect_to cart_path
+  must_redirect_to order_path
   end
 
 
@@ -38,7 +38,7 @@ describe CartsController do
   end
 
   it "should get an edit page" do
-    get edit_carts_path(orderitems(:apple).id)
+    get edit_orders_path(orderitems(:apple).id)
     must_respond_with :success
   end
 
