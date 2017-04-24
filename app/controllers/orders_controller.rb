3733202f51c58d87f527fb8637564400df178e67
Olivia <ssecretsharer@gmail.com>
2017-04-23 19:12:03 -0700

    stubbed out orders controller methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,59 @@
 class OrdersController < ApplicationController
+
+  class ProductsController < ApplicationController
+     before_action :find_product, only: [:show, :edit, :update]
+     #before_action :require_login
+
+    def index
+      if params[:merchant_id]
+        merchant = Merchant.find_by(id: params[:merchant_id])
+        @products = merchant.products
+      else
+      @products = Product.all
+      end
+    end
+
+    def show; end
+
+    def edit; end
+
+    def update
+      if @product.update product_params
+        redirect_to product_path
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      Product.destroy(params[:id])
+
+      redirect_to products_path
+    end
+
+    def new
+      @product = Product.new
+    end
+
+    def create
+      product = Product.new product_params
+      product.merchant_id = params[:merchant_id]
+      product.save
+
+      if product.save
+        redirect_to merchant_products_path(params[:merchant_id])
+      end
+    end
+
+    private
+
+    def product_params
+      params.require(:product).permit(:name, :price, :quantity, :merchant_id)
+    end
+
+    def find_merchant
+      @merchant = Merchant.find(params[:id])
+    end
+  end
+
 end
