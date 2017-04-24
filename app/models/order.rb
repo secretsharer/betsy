2ad0a9d0fc4a448007341e19ce0ab050dfc1a736
Olivia <ssecretsharer@gmail.com>
2017-04-23 19:21:29 -0700

    added validations and relationships to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,12 @@
 class Order < ApplicationRecord
+
+  class Order < ApplicationRecord
+    has_many :orderitems, :foreign_key => 'orderitems'
+    belongs_to :merchant, :foreign_key => 'merchant_id'
+
+    validates :id, presence: true, uniqueness: true
+    validates :session, presence: true
+
+  end
+
 end
