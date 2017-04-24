3860e940fe837c179954e3f1bc3b974acdfbb87a
sai samant <saisamant@gmail.com>
2017-04-23 21:41:00 -0700

    super basic controller methods

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,18 +1,27 @@
 class OrderItemsController < ApplicationController
 
   def new
+    @orderitem = Orderitem.new
   end
 
   def create
+    @orderitem.create
   end
 
   def show
+    @orderitem = Orderitem.find_by_id (params[:id])
   end
 
   def edit
+    @orderitem = Orderitem.find_by_id (params[:id])
+    if @orderitem == nil
+      render_404
+    end
   end
 
   def destroy
+    Orderitem.destroy(params[:id])
+    redirect_to #order path?
   end
 
   #we will need a method to get order items by order
@@ -22,6 +31,7 @@ class OrderItemsController < ApplicationController
   private
 
   def orderitem_params
+    params.require(:orderitem).permit(:quantity)
 
   end
 
