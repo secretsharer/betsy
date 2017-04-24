8fd93e22932ea103e1d911db9d92d7045295e8af
Olivia <ssecretsharer@gmail.com>
2017-04-23 21:55:31 -0700

    re-added sessions controller methods after losing them in the great branching accident of 2017

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,25 +4,25 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']#our request translated from github and imniauth
     #raise
-    user = User.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+    merchant = Merchant.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
 
     #if it's not there in the db then make it save it
-    if user.nil?
-      user = User.create_from_github(auth_hash)
-      if user.nil?
+    if merchant.nil?
+      merchant = Merchant.create_from_github(auth_hash)
+      if merchant.nil?
         flash[:error] = "Could not login"
         redirect_to root_path
         # else
         #   #if it is there then save some data to session then redirect
       end
-      session[:user_id] = user.id
+      session[:user_id] = merchant.id
       flash[:success] = "Logged in Successfully"
       redirect_to root_path
     end
 
     def logout
-      #session[:user_id] = nil
-      session.delete(:user_id)
+      #session[:merchant_id] = nil
+      session.delete(:merchant_id)
       flash[:success] = "You are successfully logged out."
       redirect_to root_path
     end
