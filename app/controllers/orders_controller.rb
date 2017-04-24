11acbf4d1c4ca8e9c4d642ce55103c243331f6be
jjousun <jjousun@gmail.com>
2017-04-24 11:21:05 -0700

    Working on Orders Controller methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,46 +1,43 @@
+
 class OrdersController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update]
+  #before_action :require_login
 
-  class ProductsController < ApplicationController
-    before_action :find_product, only: [:show, :edit, :update]
-    #before_action :require_login
-
-
-    def index
-    end
-
-    def new
-      @product = Product.new
-    end
-
-    def show
-      @orderitems = current_order.orderitems
-    end
-
-    def edit; end
-
-    def update
-
-    end
-
-    def destroy
-
-    end
-
-
-
-    def create
-
-    end
-
-    private
-
-    def order_params
-      params.require(:order).permit(:id, :session_id, :quantity, :merchant_id)
-    end
-
-    def find_merchant
-      @merchant = Merchant.find(params[:id])
-    end
+  def index
+    @orders = Order.all
   end
 
+  def new
+    @order = Order.new
+  end
+
+  def show
+    @order = Order.find_by_id[params(:id)]
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def create
+
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:id, :session_id, :quantity, :merchant_id)
+  end
+
+  def find_merchant
+    @merchant = Merchant.find(params[:id])
+  end
 end
