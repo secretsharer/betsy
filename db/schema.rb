fec1a1bc0274c1c945385a4d834a3285aaadb9c9
Olivia <ssecretsharer@gmail.com>
2017-04-23 21:10:09 -0700

    Made sessions full rest resources, gonna try this approach

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422143427) do
+ActiveRecord::Schema.define(version: 20170424040303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,11 @@ ActiveRecord::Schema.define(version: 20170422143427) do
     t.datetime "updated_at", null: false
     t.integer  "rating"
     t.integer  "product_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "orderitems", "orders"
