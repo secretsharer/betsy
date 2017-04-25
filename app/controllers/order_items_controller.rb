10694eab9f2c53879be502f0eee5fd3ee247a7ce
sai samant <saisamant@gmail.com>
2017-04-24 21:18:00 -0700

    edits to create orderitem

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -9,12 +9,11 @@ class OrderItemsController < ApplicationController
       @orderitem.quantity = params[:order_items][:quantity]
       @orderitem.order_id = current_order.id
       @orderitem.save
-      raise
     else
       flash[:error] = "Not enough product in stock"
     end
       flash[:success] = "Item added to cart"
-      redirect_to product_path(params[:order_items][:product_id])
+      redirect_to product_path(@orderitem.product.id)
   end
 
   def update
