ae9593255ca75fee147b0ec870a77da373a29234
Anacelia Gillette <acgillette426@gmail.com>
2017-04-25 12:52:04 -0700

    Quantity testing green

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -22,6 +22,12 @@ it "should not create an orderitem if it's already in the cart" do
 end
 
 it "should not create an orderitem is there is not enough quantity of product" do
+  proc   {
+    post order_items_path, params: { order_items:
+      { product_id: products(:apple).id,
+        quantity: 6
+   }  }
+ }.must_change 'Orderitem.count', 0
 end
 
 it "should update the quantity in the cart" do
