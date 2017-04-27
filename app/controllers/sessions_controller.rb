7100499155dbc0f37214056bf5493c7b16622be4
sai samant <saisamant@gmail.com>
2017-04-26 21:51:33 -0700

    session testing done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
     merchant = Merchant.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
-    puts "merchant: #{merchant}"
+
     # if it's not there in the db, then make/save it
     if merchant.nil?
-      puts "#{auth_hash}"
+
       merchant = Merchant.create_from_github(auth_hash)
       if merchant.nil?
         flash[:error] = "Could not log in"
@@ -19,11 +19,7 @@ class SessionsController < ApplicationController
       session[:merchant_id] = merchant.id
       flash[:success] = "Logged in successfully!"
     end
-    puts "++++++++++++++++++++++++"
-    puts "merchant: #{merchant}"
-    puts "session: #{session[:merchant_id]}"
-    puts "#{flash[:error]}"
-    puts "#{flash[:success]}"
+
     redirect_to root_path
   end
 
