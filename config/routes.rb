fefc7be424354aa1f76e9de297f9997d4754f1da
sai samant <saisamant@gmail.com>
2017-04-27 09:47:38 -0700

    fixed routing error for merchant creating new product

--- config/routes.rb
+++ config/routes.rb
@@ -35,10 +35,7 @@ Rails.application.routes.draw do
   # get "/:carts/show/payment/", to: "payments#payment", as: "new_payment"
   # patch "/:carts/show/payment/", to: "payments#update", as: "update_cart_payment"
 
-  resources :payments, only: [:new, :create]
-
-  get "/:carts/show/payment/", to: "payments#payment", as: "new_payment"
-  patch "/:carts/show/payment/", to: "payments#update", as: "cart_update_payment"
+  
   get "/carts/show/payment/", to: "payments#new", as: "new_payment"
   patch "/carts/show/payment/", to: "payments#update", as: "update_payment"
 
